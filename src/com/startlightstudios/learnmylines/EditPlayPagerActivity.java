e559cb3d985a18cda5e487cab597b1d80251f0b7
Lucas <lconti97@gmail.com>
2014-08-29 17:29:22 -0400

    Added unresponsive tabs

--- src/com/startlightstudios/learnmylines/EditPlayPagerActivity.java
+++ src/com/startlightstudios/learnmylines/EditPlayPagerActivity.java
@@ -1,5 +1,11 @@
 package com.startlightstudios.learnmylines;
 
+import android.annotation.TargetApi;
+import android.app.ActionBar;
+import android.app.ActionBar.TabListener;
+import android.app.FragmentTransaction;
+import android.app.ActionBar.Tab;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -9,6 +15,7 @@ import android.support.v4.view.ViewPager;
 
 import com.example.learnmylines.R;
 
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class EditPlayPagerActivity extends FragmentActivity {
 	private ViewPager mViewPager;
 	
@@ -16,6 +23,38 @@ public class EditPlayPagerActivity extends FragmentActivity {
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+		final ActionBar actionBar = getActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		ActionBar.TabListener tabListener = new TabListener() {
+			
+			@Override
+			public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onTabSelected(Tab tab, FragmentTransaction ft) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onTabReselected(Tab tab, FragmentTransaction ft) {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+		actionBar.addTab(
+				actionBar.newTab() 
+					.setText(R.string.edit)
+					.setTabListener(tabListener));
+		
+		actionBar.addTab(
+				actionBar.newTab() 
+					.setText(R.string.play)
+					.setTabListener(tabListener));
+		
 		setContentView(R.layout.activity_edit_play_pager);
 		mViewPager = (ViewPager)findViewById(R.id.activity_edit_play_view_pager);
 		
