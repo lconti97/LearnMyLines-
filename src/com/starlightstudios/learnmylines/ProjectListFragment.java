9e3e42ab7cea76618863f1db5753edb90c941fb5
Lucas <lconti97@gmail.com>
2014-09-03 00:02:34 -0400

    Add TextView on Project click

--- src/com/starlightstudios/learnmylines/ProjectListFragment.java
+++ src/com/starlightstudios/learnmylines/ProjectListFragment.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ProjectListFragment extends ListFragment {
 	private ArrayList<Project> mProjects;
@@ -21,6 +25,19 @@ public class ProjectListFragment extends ListFragment {
 						mProjects);
 		
 		setListAdapter(adapter);
+	}
+	
+	@Override
+	public void onListItemClick(ListView l, View v, int pos, long id)
+	{
+		Project p = (Project) getListAdapter().getItem(pos);
+		TextView tv = new TextView(getActivity());
+		tv.setText("Scene I");
+		LayoutParams params = new LayoutParams(
+									LayoutParams.WRAP_CONTENT,
+									LayoutParams.WRAP_CONTENT);
+		
+		getActivity().addContentView(tv, params);
 	}
 
 }
