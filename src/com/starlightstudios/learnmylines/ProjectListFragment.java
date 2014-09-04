be412724a87c8b04cd6b18e36dced611d2113ddb
Lucas <lconti97@gmail.com>
2014-09-03 23:57:42 -0400

    Change ProjectListFragment's view
    Displays a TextView and ListView

--- src/com/starlightstudios/learnmylines/ProjectListFragment.java
+++ src/com/starlightstudios/learnmylines/ProjectListFragment.java
@@ -2,25 +2,32 @@ package com.starlightstudios.learnmylines;
 
 import java.util.ArrayList;
 
-import android.os.Bundle;
-import android.support.v4.app.ListFragment;
-import android.widget.ArrayAdapter;
+import com.example.learnmylines.R;
 
-public class ProjectListFragment extends ListFragment {
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ProjectListFragment extends Fragment {
 	private ArrayList<Project> mProjects;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		mProjects = ProjectManager.get().getProjects();
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup parent, 
+			Bundle savedInstanceState)
+	{
+		View v = inflater.inflate(R.layout.fragment_project_list, null);
 		
-		ArrayAdapter<Project> adapter = 
-				new ArrayAdapter<Project>(getActivity(),
-						android.R.layout.simple_list_item_1,
-						mProjects);
-		
-		setListAdapter(adapter);
+		return v;
 	}
 
 }
