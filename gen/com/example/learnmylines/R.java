ba292151a9bc746415d92bff28901978f64bd867
Lucas <lconti97@gmail.com>
2014-09-06 23:14:10 -0400

    User can add Projects

--- gen/com/example/learnmylines/R.java
+++ gen/com/example/learnmylines/R.java
@@ -1091,7 +1091,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050043;
+        public static final int action_settings=0x7f050045;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int activity_edit_play_view_pager=0x7f05003c;
         public static final int always=0x7f05000b;
@@ -1100,6 +1100,7 @@ containing a value of this type.
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
+        public static final int dialog_project_title_editText=0x7f05003e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050030;
@@ -1107,17 +1108,18 @@ containing a value of this type.
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
         public static final int fragmentContainer=0x7f05003d;
-        public static final int fragment_scene_edit_recordButton=0x7f05003f;
-        public static final int fragment_scene_play_playButton=0x7f050040;
+        public static final int fragment_scene_edit_recordButton=0x7f050040;
+        public static final int fragment_scene_play_playButton=0x7f050041;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int listItemContainer=0x7f050041;
+        public static final int listItemContainer=0x7f050042;
         public static final int listMode=0x7f050001;
-        public static final int listView1=0x7f05003e;
+        public static final int listView1=0x7f05003f;
         public static final int list_item=0x7f050029;
+        public static final int menu_item_new_project=0x7f050044;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
@@ -1142,7 +1144,7 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textView=0x7f050042;
+        public static final int textView=0x7f050043;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1201,14 +1203,16 @@ containing a value of this type.
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_edit_play_pager=0x7f030018;
         public static final int activity_fragment=0x7f030019;
-        public static final int fragment_project_list=0x7f03001a;
-        public static final int fragment_scene_edit=0x7f03001b;
-        public static final int fragment_scene_play=0x7f03001c;
-        public static final int list_item=0x7f03001d;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001e;
+        public static final int dialog_title=0x7f03001a;
+        public static final int fragment_project_list=0x7f03001b;
+        public static final int fragment_scene_edit=0x7f03001c;
+        public static final int fragment_scene_play=0x7f03001d;
+        public static final int list_item=0x7f03001e;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001f;
     }
     public static final class menu {
-        public static final int scene_play=0x7f0c0000;
+        public static final int fragment_project_list=0x7f0c0000;
+        public static final int scene_play=0x7f0c0001;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1254,8 +1258,10 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000e;
         public static final int app_name=0x7f0a000d;
         public static final int edit=0x7f0a0011;
+        public static final int new_project=0x7f0a0014;
         public static final int pause=0x7f0a0010;
         public static final int play=0x7f0a000f;
+        public static final int project_title=0x7f0a0015;
         public static final int record=0x7f0a0012;
         public static final int stop=0x7f0a0013;
     }
--- src/com/starlightstudios/learnmylines/ProjectListFragment.java
+++ src/com/starlightstudios/learnmylines/ProjectListFragment.java
@@ -2,11 +2,17 @@ package com.starlightstudios.learnmylines;
 
 import java.util.ArrayList;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -18,7 +24,9 @@ import com.example.learnmylines.R;
 
 public class ProjectListFragment extends Fragment {
 	private static final String TAG = "ProjectListFragment";
-	
+	private static final String DIALOG_TITLE = "title";
+	private static final int REQUEST_TITLE = 0;
+
 	private ArrayList<NLevelItem> mList;
 	private ListView mListView;
 	private ProjectManager mManager;
@@ -28,6 +36,7 @@ public class ProjectListFragment extends Fragment {
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
 	}
 
 	@Override
@@ -38,9 +47,72 @@ public class ProjectListFragment extends Fragment {
 
 		//TODO: Change listView1's name
 		mListView = (ListView)v.findViewById(R.id.listView1);
-		mList = new ArrayList<NLevelItem>();
 		mManager = ProjectManager.get();
 
+		updateList();
+
+
+		mListView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+
+				NLevelItem temp = (NLevelItem) mAdapter.filtered.get(arg2);
+				if (temp.getWrappedObject() instanceof Scene)
+				{
+					Intent i = new Intent(getActivity(), EditPlayPagerActivity.class);
+					getActivity().startActivity(i);
+				}
+				mAdapter.toggle(arg2);
+				mAdapter.getFilter().filter();
+
+			}
+		});
+		return v;
+	}
+
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
+	{
+		super.onCreateOptionsMenu(menu, inflater);
+		inflater.inflate(R.menu.fragment_project_list, menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch(item.getItemId())
+		{
+		case R.id.menu_item_new_project:
+			FragmentManager fm = getActivity()
+			.getSupportFragmentManager();
+			ProjectTitleDialog dialog = new ProjectTitleDialog();
+			dialog.setTargetFragment(ProjectListFragment.this, REQUEST_TITLE);
+			dialog.show(fm, DIALOG_TITLE);
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		if(resultCode != Activity.RESULT_OK) return;
+		if(requestCode == REQUEST_TITLE)
+		{
+			Project p = new Project(
+					data.getStringExtra(ProjectTitleDialog.EXTRA_TITLE),
+					mManager);
+			mManager.addProject(p);
+			
+			updateList();
+		}
+	}
+
+	private void updateList()
+	{
+		mList = new ArrayList<NLevelItem>();
 		for(int i = 0; i < mManager.getProjects().size(); i++)
 		{
 			final Project project = mManager.getProjects().get(i);
@@ -62,7 +134,7 @@ public class ProjectListFragment extends Fragment {
 						}
 					});		
 			mList.add(grandParent);
-			
+
 			ArrayList<Act> acts = project.getActs();
 			for(int j = 0; j < acts.size(); j++)
 			{
@@ -71,20 +143,20 @@ public class ProjectListFragment extends Fragment {
 						act,
 						grandParent,
 						new NLevelView() {
-					
-					@Override
-					public View getView(NLevelItem item) {
-						View view = getActivity().getLayoutInflater().
-								inflate(R.layout.list_item, null);
-						TextView tv = (TextView) view.findViewById(R.id.textView);
-						String name = act.getTitle();
-						tv.setText("\t\t" + name);
-						return view;
-					}
-				});
-		
+
+							@Override
+							public View getView(NLevelItem item) {
+								View view = getActivity().getLayoutInflater().
+										inflate(R.layout.list_item, null);
+								TextView tv = (TextView) view.findViewById(R.id.textView);
+								String name = act.getTitle();
+								tv.setText("\t\t" + name);
+								return view;
+							}
+						});
+
 				mList.add(parent);
-				
+
 				ArrayList<Scene> scenes = act.getScenes();
 				for(int n = 0; n < scenes.size(); n++)
 				{
@@ -93,44 +165,25 @@ public class ProjectListFragment extends Fragment {
 							scene,
 							parent,
 							new NLevelView() {
-						
-						@Override
-						public View getView(NLevelItem item) {
-							View view = getActivity().getLayoutInflater()
-									.inflate(R.layout.list_item, null);
-							TextView tv = (TextView) view.findViewById(R.id.textView);
-							String name = scene.getTitle();
-							tv.setText("\t\t\t\t" + name);
-							return view;
-						}
-					});
-				
+
+								@Override
+								public View getView(NLevelItem item) {
+									View view = getActivity().getLayoutInflater()
+											.inflate(R.layout.list_item, null);
+									TextView tv = (TextView) view.findViewById(R.id.textView);
+									String name = scene.getTitle();
+									tv.setText("\t\t\t\t" + name);
+									return view;
+								}
+							});
+
 					mList.add(child);
 				}
 			}
 
 		}
-
+		
 		mAdapter = new NLevelAdapter(mList);
 		mListView.setAdapter(mAdapter);
-		mListView.setOnItemClickListener(new OnItemClickListener() {
-
-			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
-				
-				NLevelItem temp = (NLevelItem) mAdapter.filtered.get(arg2);
-				if (temp.getWrappedObject() instanceof Scene)
-				{
-					Intent i = new Intent(getActivity(), EditPlayPagerActivity.class);
-					getActivity().startActivity(i);
-				}
-				mAdapter.toggle(arg2);
-				mAdapter.getFilter().filter();
-
-			}
-		});
-		return v;
 	}
-
 }
